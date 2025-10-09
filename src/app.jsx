bbb2aab70d1ebede4d39f347ececa1a55eae0886
Logan <loganx@byu.edu>
2025-10-08 18:51:27 -0600

    Added component stubs

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Game } from './game/game';
+import { Scores } from './scores/scores';
+
 export default function App() {
   return ( 
   <div className="body bg-dark text-light">A
