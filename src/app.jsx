8526b522bba5a2e8ad2232eb143a94af5a5d38ee
Logan <loganx@byu.edu>
2025-10-08 19:16:43 -0600

    Added the router functionality Including NavLink elements and Routes

--- src/app.jsx
+++ src/app.jsx
@@ -9,28 +9,39 @@ import { Scores } from './scores/scores';
 
 export default function App() {
   return ( 
-  <div className="body bg-dark text-light">A
-    <header className="container-fluid fixed-top">
-        <nav className="navbar navbar-light">
-            <a className="navbar-brand" href="#">
-                <img id="title-bar" src="madamfo.png" alt="Madamfo" height="100" />
-            </a>
-            <menu className="navbar-brand">
-                <li className="nav-item"><a className="nav-link active" href="index.html"> Home </a></li>
-                <li className="nav-item"><a className="nav-link" href="game.html">Play</a></li>
-                <li className="nav-item"><a className="nav-link" href="score.html">Scoreboard</a></li>
-            </menu>
-        </nav>
-    </header>
+    <BrowserRouter>
+        <div className="body bg-dark text-dark">A
+            <header className="container-fluid fixed-top">
+                <nav className="navbar navbar-light">
+                    <a className="navbar-brand" href="#">
+                        <img id="title-bar" src="madamfo.png" alt="Madamfo" height="100" />
+                    </a>
+                    <menu className="navbar-brand">
+                        <li className="nav-item"><NavLink className="nav-link" to=""> Home </NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="game">Play</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="scores">Scoreboard</NavLink></li>
+                    </menu>
+                </nav>
+            </header>
 
-    <main>App components go here</main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/game' element={<Game />} />
+                <Route path='/scores' element={<Scores />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-    <footer className="text-white-50">
-        <div>
-            <span className="text_reset">Logan Smith</span>
-            <a href="https://github.com/Ljreds/virtual-spider.git">My GitHub</a>
+            <footer className="text-white">
+                <div>
+                    <span className="text_reset">Logan Smith</span>
+                    <a href="https://github.com/Ljreds/virtual-spider.git">My GitHub</a>
+                </div>
+            </footer>
         </div>
-    </footer>
-  </div>
+    </BrowserRouter>
   )
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
