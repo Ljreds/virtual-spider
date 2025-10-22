4a911f1e7773815f1eaaf94cdc466e218f3bd8ea
Logan <loganx@byu.edu>
2025-10-21 20:23:35 -0600

    Worked on getting the storage working

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import './login.css';
 
-export function Login() {
+import Button from 'react-bootstrap/Button';
 
-    const [userName, setUserName] = React.useState(userName);
-    const [password, setPassword] = React.useState('');
-    const [displayError, setDisplayError] = React.useState(null);
 
-    class User{
-        constructor(name, password) {
-            this.username = name;
-            this.password = password;
-
-        }
+class User{
+    constructor(name, password) {
+        this.username = name;
+        this.password = password;
 
     }
+
+}
+
+
+export function Login() {
+
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
 
   async function loginUser() {
@@ -36,20 +39,22 @@ export function Login() {
             <div className = "right">
                 <h1 id="login-title">Akwaaba</h1>
                 <h2 id="login-subtitle">(Welcome)</h2>
-                <form method = "get" action="game" >
-                    <div className="input-group mb-3">
-                        <label for="login" className="input-group-text">Username: </label>
-                        <input type="text" id="login" placeholder="Your name here" className="form-control" onChange={(e) => setUserName(e.target.value)} />
-                    </div>
-                    <div className="input-group mb-3">
-                        <label for="password" className="input-group-text">Password:    </label>
-                        <input type="password" id="password" placeholder="Password" className="form-control" onChange={(e) => setPassword(e.target.value)}/>
-                    </div>
-                    <div id="login-button">
-                        <button type="submit" className="btn btn-primary" onClick={() => loginUser()} disabled={!userName || !password}>Login</button>
-                        <button type="submit" className="btn btn-secondary"  onClick={() => signUp()} disabled={!userName || !password}>Signup</button>
-                    </div>
-                </form>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Username: </span>
+                    <input type="text" className="form-control" onChange={(e) => setUserName(e.target.value)} placeholder="Username"/>
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Password: </span>
+                    <input type="password"  className="form-control" onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+                </div>
+                <div id="login-button">
+                    <Button variant="primary" onClick={() => loginUser()} disabled={!userName || !password}>
+                        Login
+                    </Button>
+                    <Button variant="secondary"  onClick={() => signUp()} disabled={!userName || !password}>
+                        Signup
+                    </Button>
+                </div>
             </div>
         </div>
     </main>
