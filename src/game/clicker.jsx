37bd19b440f03aaf875f19d5ffb87f48f958420c
Logan <loganx@byu.edu>
2025-10-27 16:35:04 -0600

    Set up the clicker

--- src/game/clicker.jsx
+++ src/game/clicker.jsx
@@ -6,10 +6,32 @@ import Button from 'react-bootstrap/Button';
 
 export function Clicker(props) {
 
-    const [hunger, setHunger] = React.useState(50);
+    const [sate, setSate] = React.useState(0);
+    const [sateMult, setSateMult] = React.useState(20);
+
     const [dirt, setDirt] = React.useState(50);
+    const [score, setScore] = React.useState(0);
+    const [mod, setModifier] = React.useState(1);
 
     const clamp = v => Math.max(0, Math.min(100, v));
+
+    React.useEffect(() => {
+        if(sate === 100){
+            console.log('sate reached 100');
+            setModifier(m => m * 2);
+            setSate(0);
+            setSateMult(m => m - 1);
+        }
+    });
+
+    React.useEffect(() => {
+        const id = setInterval(() => {
+            setDirt(d => clamp(d + 2));
+        }, 1000)
+        return () => clearInterval(id);
+    }, []);
+
+    
 
   return (
    <main className="container-fluid text-white">
@@ -32,21 +54,23 @@ export function Clicker(props) {
         
             <div className="row justify-content-md-center">
                 <div className="col-md-auto">
-                    <p className="score-container">Happiness: <output id="score">999999</output></p>
+                    <p className="score-container">Happiness: <output id="score">{score}</output></p>
                 </div>
             </div>
 
             <div className="row">
                 <div className=" col">
                     <div className="meter-container justify-content-end">
-                        <span className="meter-name">Hunger</span>
+                        <span className="meter-name">Sate</span>
                         <div className="game-meter">
-                            <div className="meter-fill" style={{ height: `${hunger}%` }} aria-valuenow={hunger} aria-valuemin={0} aria-valuemax={100}></div>
+                            <div className="meter-fill" style={{ height: `${sate}%` }} aria-valuenow={sate} aria-valuemin={0} aria-valuemax={100}></div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col avatar"><img src="goat.png" alt="Aponkye(goat)" width="544" /></div>
+                <div className="col avatar">
+                    <img src="goat.png" alt="Aponkye(goat)" width="544" onClick= {() => setScore(s => s + mod)} style={{cursor: 'pointer'}}/>
+                </div>
 
                 <div className ="col">
                     <div className="meter-container">
@@ -60,7 +84,7 @@ export function Clicker(props) {
             </div>
             <div className="row">
                 <div className="col feed-btn">
-                    <Button className="game-button" type="button"  onClick={() => setHunger(h => clamp(hunger - 2))}>
+                    <Button className="game-button" type="button"  onClick={() => setSate(h => clamp(sate + sateMult))}>
                         Feed
                     </Button>
                 </div>
