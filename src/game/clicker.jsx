578e661e9f749155e631593fbbbbb333e9763a2f
Logan <loganx@byu.edu>
2025-10-28 16:39:37 -0600

    Impleted the score saving using the simon code for now, hoping to come up with a method better suited to my code in coming git pushes

--- src/game/clicker.jsx
+++ src/game/clicker.jsx
@@ -37,8 +37,33 @@ export function Clicker(props) {
 
     function saveScore(score) {
         const newScore = {name: userName, score: score}
-
         
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        
+        if (scoresText) {
+        scores = JSON.parse(scoresText);
+        }
+
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+            if (newScore.score > prevScore.score) {
+                scores.splice(i, 0, newScore);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+        scores.push(newScore);
+        }
+
+        if (scores.length > 10) {
+        scores.length = 10;
+        }
+
+        localStorage.setItem('scores', JSON.stringify(scores));
+
     }
 
     const dirtRef = React.useRef(dirt);
