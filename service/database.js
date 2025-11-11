eab9164b892a01f86124a285605245dd8e5d48a7
Logan <loganx@byu.edu>
2025-11-11 15:04:11 -0700

    Added all of my user updating/finding functions in my database.js

--- service/database.js
+++ service/database.js
@@ -5,4 +5,20 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userDatabase = db.collection('users');
-const scoreDatabase = db.collection('scores')
+const scoreDatabase = db.collection('scores');
+
+async function setUser(user) {
+    await userDatabase.insertOne(user);
+}
+
+function findUser(userName) {
+    return userDatabase.findOne({userName: userName});
+}
+
+function findUserByToken(token) {
+    return userDatabase.findOne({token: token})
+}
+
+async function updateUser(user) {
+    await userDatabase.updateOne({userName: user.userName}, { $set: user })
+}
