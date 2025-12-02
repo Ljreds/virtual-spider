d7d4835d3257ee73eb1dcf69e6492a8887d6423b
Logan <loganx@byu.edu>
2025-12-01 17:02:42 -0700

    Added a message function to send data to other clients

--- service/websocket.js
+++ service/websocket.js
@@ -3,4 +3,19 @@ const { WebSocketServer} = require('wasi');
 function websocket(httpServer) {
   const socketServer = new WebSocketServer({ server: httpServer });
 
+  socketServer.on('connection', (socket) => {
+    socket.isAlive = true;
+
+    socket.on('message', function message(data) {
+      socketServer.clients.forEach((client) => {
+        if (client !== socket && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    });
+  });
+    
+
 }
+
+module.exports = { websocket };
